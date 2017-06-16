e720a5bad73b599cbe933da156b742d107246ffe
iyegoroff <iegoroff@gmail.com>
2017-06-16 04:40:13 +0300

    shrinkIfEmpty

--- src/index.d.ts
+++ src/index.d.ts
@@ -4,6 +4,7 @@ import {TextInputProperties} from 'react-native';
 export interface AutoGrowTextInputProps extends TextInputProperties {
   readonly maxHeight?: number;
   readonly minHeight?: number;
+  readonly shrinkIfEmpty?: boolean;
 }
 
 export declare class AutoGrowTextInput extends React.Component<
